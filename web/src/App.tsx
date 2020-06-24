f520696692c818c340057178fbce369bfb41e466
Douglas Mendes <capudougmor@gmail.com>
2020-06-23 21:57:17 -0300

    Criada a pagina Home

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 import './App.css';
 
-import Home from './pages/Home'
+import Routes from './routes'
 
 
 function App() {
 
 
   return (
-   <Home />
+   <Routes /> 
   );
 }
 
